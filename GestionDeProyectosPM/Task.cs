dbba34059e47b29f5ff04ae7b70f1edd5060b0f5
Matias Moreira <moreiramatias10@hotmail.com>
2020-01-26 03:40:01 -0300

    It was added to throw an exeption when estimate is not a number that is in the fibonacci series

--- GestionDeProyectosPM/Task.cs
+++ GestionDeProyectosPM/Task.cs
@@ -1,10 +1,34 @@
 using System;
+using System.Collections.Generic;
+
 namespace GestionDeProyectosPM
 {
     public class Task
     {
+        //Campos
+        private int _estimate;
+
+        
+        //Constructores
+        public Task()
+        {
+            _estimate = 0;
+        }
+
+
         //Propiedadades
-        public int Estimate { get; set; }
+        public int Estimate
+        {
+            get {
+                return _estimate;
+            }
+            set {
+                
+                _estimate = checkFibonacci(value);
+            }
+        }
+
+        //public int Estimate { get; set; }
 
         public UserStory UserStory { get; set; }
 
@@ -15,6 +39,20 @@ namespace GestionDeProyectosPM
         public virtual float getHours()
         {
             return 0;
+        }
+
+        private int checkFibonacci(int value)
+        {
+            if (!this.getSerieFibonacci().Exists(item => item == value) )
+            {
+                throw new System.ArgumentException("Debe ser un numero perteciente a la serie de Fibonacci", "Estimacíon");
+            }
+            return value;
+        }
+        private List<int> getSerieFibonacci()
+        {
+            List<int> _serie = new List<int>(new int[] { 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 });
+            return _serie;
         }
         /*
         public float getHours()
